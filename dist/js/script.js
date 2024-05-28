2745c4e6dae1d8872c9d4a8aa51207ba8c7ef616
zakogoto <116558645+zakogoto@users.noreply.github.com>
2024-05-28 15:15:48 +0300

    fixed bug with unavalible burger

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,8 +6,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
 	// });
 
 	let sections = gsap.utils.toArray('section');
+	let subtitleItems = gsap.utils.toArray('.title_fz16');
+	let titleItems = gsap.utils.toArray('.title_fz36');
 	sections.forEach((section, i) => {
-		console.log(section.offsetHeight, window.screen.availHeight)
+		// console.log(section.offsetHeight, window.screen.availHeight)
+		gsap.fromTo(section, {opacity: 1}, {
+			opacity: 0,
+			scrollTrigger:{
+			trigger: section,
+			// markers: true,
+			start: 'center',
+			end: '150%',
+			scrub: true,}
+		})
 		ScrollTrigger.create({
 			ease: true,
 			trigger: section,
@@ -17,8 +28,35 @@ document.addEventListener("DOMContentLoaded", (event) => {
 			pin: true,
 			// end: '+=100px',
 			scrub: true,
-			snap: 1,
+			snap: 0.01,
 			pinSpacing: false
+		})
+	})
+
+	subtitleItems.forEach(item => {
+		gsap.fromTo(item, {opacity: 0,  x: -100}, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				// markers: true,
+				start: '0',
+				// end: '300',
+				scrub: true,
+				snap: 0.1
+			}
+		})
+	})
+	titleItems.forEach(item => {
+		gsap.fromTo(item, {opacity: 0,  x: 300}, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				// markers: true,
+				start: '0',
+				// end: '30%',
+				scrub: true,
+				snap: 0.1
+			}
 		})
 	})
    });
@@ -48,6 +86,7 @@ const menuActions = () => {
 		item.addEventListener('click', () => {
 			menu.classList.remove("active");
 			document.body.style.overflowY = 'scroll';
+			hamburger.style.display = 'flex';
 		});
 	});
 	
@@ -58,40 +97,40 @@ const menuActions = () => {
 
 menuActions();
 
-function parallax (w, l) {
-	const wrapper = document.querySelector(w);
-	const layers = document.querySelectorAll(l);
+// function parallax (w, l) {
+// 	const wrapper = document.querySelector(w);
+// 	const layers = document.querySelectorAll(l);
 	
-	function initParallax (e) {
-		const clientX = e.clientX;
-		const clientY = e.clientY;
-		const parallaxLeftOffset = wrapper.getBoundingClientRect().left;
-		const parallaxTopOffset = wrapper.getBoundingClientRect().top;
-		// const parallaxOverflow = parallaxTopOffset / wrapper.getBoundingClientRect().height;
-		const coordX = clientX - parallaxLeftOffset - 0.5 * wrapper.offsetWidth;
-		const coordY = clientY - parallaxTopOffset - 0.5 * wrapper.offsetHeight;
-		// console.log(wrapper.getBoundingClientRect().height + parallaxTopOffset);
-		// wrapper.addEventListener('scroll', (e) => {
-		// 	if(wrapper.getBoundingClientRect().height + parallaxTopOffset <= wrapper.getBoundingClientRect().height / 2) {
-		// 	}
-		// })
+// 	function initParallax (e) {
+// 		const clientX = e.clientX;
+// 		const clientY = e.clientY;
+// 		const parallaxLeftOffset = wrapper.getBoundingClientRect().left;
+// 		const parallaxTopOffset = wrapper.getBoundingClientRect().top;
+// 		// const parallaxOverflow = parallaxTopOffset / wrapper.getBoundingClientRect().height;
+// 		const coordX = clientX - parallaxLeftOffset - 0.5 * wrapper.offsetWidth;
+// 		const coordY = clientY - parallaxTopOffset - 0.5 * wrapper.offsetHeight;
+// 		// console.log(wrapper.getBoundingClientRect().height + parallaxTopOffset);
+// 		// wrapper.addEventListener('scroll', (e) => {
+// 		// 	if(wrapper.getBoundingClientRect().height + parallaxTopOffset <= wrapper.getBoundingClientRect().height / 2) {
+// 		// 	}
+// 		// })
 		
-		// if()
-		// wrapper.setAttribute('style', `opacity:${1 + (parallaxTopOffset / wrapper.getBoundingClientRect().height)};`)
+// 		// if()
+// 		// wrapper.setAttribute('style', `opacity:${1 + (parallaxTopOffset / wrapper.getBoundingClientRect().height)};`)
 		
-		layers.forEach(layer => {
-			const layerSpeed = layer.dataset.speed;
-			const x = (coordX * layerSpeed).toFixed(2);
-			const y = (coordY * layerSpeed).toFixed(2);
-			// layer.setAttribute('style', `transform: translate(${x}px, ${y}px); opacity:${1 + parallaxOverflow};`);
-			layer.setAttribute('style', `transform: translate(${x}px, ${y}px);`);
-		})
-	}
-	wrapper.addEventListener('mousemove', initParallax);
-	// wrapper.addEventListener('mouseout', reset);
-};
+// 		layers.forEach(layer => {
+// 			const layerSpeed = layer.dataset.speed;
+// 			const x = (coordX * layerSpeed).toFixed(2);
+// 			const y = (coordY * layerSpeed).toFixed(2);
+// 			// layer.setAttribute('style', `transform: translate(${x}px, ${y}px); opacity:${1 + parallaxOverflow};`);
+// 			layer.setAttribute('style', `transform: translate(${x}px, ${y}px);`);
+// 		})
+// 	}
+// 	wrapper.addEventListener('mousemove', initParallax);
+// 	// wrapper.addEventListener('mouseout', reset);
+// };
 
-parallax('.promo', '.parallax_layer');
+// parallax('.promo', '.parallax_layer');
 
 // function parallaxScroll(next) {
 // 	const nextSec = document.querySelector(next);
